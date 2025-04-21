0cbfe9da9d3d7b5f2e840a671157eadf7c466044
cuantagua <69112155+cuantagua@users.noreply.github.com>
2025-04-20 23:02:46 -0500

    Add files via upload

--- bot/config.py
+++ bot/config.py
@@ -1,6 +1,5 @@
-from bot.config import API_TOKEN
-
 ADMIN_CHAT_ID = [851194595]
 COUPON_FORMAT = "COUPON-{id}-{timestamp}"
 FILE_STORAGE_PATH = "path/to/store/files"
 DATABASE_PATH = "path/to/database.csv"
+API_TOKEN = "your-telegram-bot-token"
