8b31585ab09bea4c2f09669046c89148363e923d
cuantagua <69112155+cuantagua@users.noreply.github.com>
2025-04-20 23:00:49 -0500

    Add files via upload

--- bot/config.py
+++ bot/config.py
@@ -1,4 +1,5 @@
-API_TOKEN = '7987679597:AAHK4k-8kzUmDBfC9_R1cVroDqXEDqz6sB4'
+from bot.config import API_TOKEN
+
 ADMIN_CHAT_ID = [851194595]
 COUPON_FORMAT = "COUPON-{id}-{timestamp}"
 FILE_STORAGE_PATH = "path/to/store/files"
--- bot/main.py
+++ bot/main.py
@@ -1,11 +1,17 @@
+import sys
+import os
+
+# Añade el directorio raíz del proyecto al sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
-from bot.config import TOKEN
+from bot.config import API_TOKEN
 from bot.handlers.user_handlers import user_command_handlers
 from bot.handlers.admin_handlers import admin_command_handlers
 
 def main():
-    updater = Updater(TOKEN, use_context=True)
+    updater = Updater(API_TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
     # Register user command handlers
