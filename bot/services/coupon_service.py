d328831a8185b893d42b519468b156e62d1cf941
cuantagua <69112155+cuantagua@users.noreply.github.com>
2025-04-20 22:42:45 -0500

    Add files via upload

--- bot/services/coupon_service.py
+++ bot/services/coupon_service.py
@@ -1,40 +1,34 @@
-from datetime import datetime, timedelta
-import random
-import string
-
-class Coupon:
-    def __init__(self, code, expiration_date):
-        self.code = code
-        self.expiration_date = expiration_date
-        self.redeemed = False
+from uuid import uuid4
+import datetime
 
 class CouponService:
     def __init__(self):
-        self.coupons = []
+        self.coupons = {}
 
-    def generate_coupon(self, length=10, validity_days=30):
-        code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
-        expiration_date = datetime.now() + timedelta(days=validity_days)
-        coupon = Coupon(code, expiration_date)
-        self.coupons.append(coupon)
-        return coupon
+    def generate_coupon(self, user_id):
+        coupon_code = str(uuid4())
+        expiration_date = datetime.datetime.now() + datetime.timedelta(days=30)
+        self.coupons[coupon_code] = {
+            'user_id': user_id,
+            'valid': True,
+            'expiration_date': expiration_date
+        }
+        return coupon_code
 
-    def validate_coupon(self, code):
-        for coupon in self.coupons:
-            if coupon.code == code:
-                if not coupon.redeemed and coupon.expiration_date > datetime.now():
-                    return True
-                break
-        return False
+    def redeem_coupon(self, coupon_code, user_id):
+        if coupon_code in self.coupons:
+            coupon = self.coupons[coupon_code]
+            if coupon['valid'] and coupon['user_id'] == user_id:
+                if datetime.datetime.now() < coupon['expiration_date']:
+                    coupon['valid'] = False
+                    return True  # Coupon redeemed successfully
+                else:
+                    return False  # Coupon expired
+            else:
+                return False  # Coupon invalid or not owned by user
+        return False  # Coupon does not exist
 
-    def redeem_coupon(self, code):
-        for coupon in self.coupons:
-            if coupon.code == code:
-                if not coupon.redeemed and coupon.expiration_date > datetime.now():
-                    coupon.redeemed = True
-                    return True
-                break
-        return False
-
-    def get_all_coupons(self):
-        return self.coupons
+    def check_coupon_status(self, coupon_code):
+        if coupon_code in self.coupons:
+            return self.coupons[coupon_code]
+        return None  # Coupon does not exist
