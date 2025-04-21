9f680561bb973b7d0f3c4d063bbe2464482380b8
cuantagua <69112155+cuantagua@users.noreply.github.com>
2025-04-20 23:16:43 -0500

    Add files via upload

--- bot/main.py
+++ bot/main.py
@@ -4,26 +4,25 @@ import os
 # Añade el directorio raíz del proyecto al sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Application
 from bot.config import API_TOKEN
 from bot.handlers.user_handlers import user_command_handlers
 from bot.handlers.admin_handlers import admin_command_handlers
 
 def main():
-    updater = Updater(API_TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
+    # Crea la aplicación
+    application = Application.builder().token(API_TOKEN).build()
 
-    # Register user command handlers
+    # Registra los handlers de usuario
     for handler in user_command_handlers:
-        dispatcher.add_handler(handler)
+        application.add_handler(handler)
 
-    # Register admin command handlers
+    # Registra los handlers de administrador
     for handler in admin_command_handlers:
-        dispatcher.add_handler(handler)
+        application.add_handler(handler)
 
-    updater.start_polling()
-    updater.idle()
+    # Inicia el bot
+    application.run_polling()
 
 if __name__ == '__main__':
     main()
